bf33ef98ae7b4ea56aa4b991f8a66c35dbcb2213
Titan Snow <sweeto@live.cn>
2016-07-14 09:58:00 +0800

    add plugin "reload"

---
+++ jbrowPlugins/reload.js
@@ -0,0 +1,32 @@
+/**
+ * Created by ts on 7/14/16.
+ */
+var events = require("events");
+var emitter = new events.EventEmitter();
+exports.onmessage = function (e) {
+    emitter.emit(e.type, e);
+};
+
+emitter.addListener("ready", function (e) {
+    var con = e.getContext();
+    var win = con.getWindow();
+    var doc = win.document;
+    var url_wrapper = doc.getElementsByClassName("url-wrapper")[0];
+    var url_form = doc.getElementById("url").parentNode;
+    var button = doc.createElement("button");
+    button.classList.add("pluginReload-reload-btn");
+    button.appendChild(doc.createTextNode("Reload"));
+    button.addEventListener("click", function () {
+        con.getContentById(con.getFocusedPageId()).contentWindow.location.reload();
+    });
+    url_wrapper.insertBefore(button, url_form);
+    con.importCSS("./jbrowPlugins/reload.css");
+});
+
+emitter.addListener("pluginMgrOption", function (e) {
+    e.window.alert("Here is NOTHING");
+});
+
+emitter.addListener("pluginMgrInfo", function (e) {
+    e.setReturnValue("Reload adds the feature of reloading pages. ");
+});
